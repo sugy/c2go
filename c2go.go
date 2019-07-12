224694952bd5da5850cc8c30fd0db25798b2e12d
sugy <sugy.kz@gmail.com>
2019-07-12 11:23:54 +0900

    tmp

--- c2go.go
+++ c2go.go
@@ -25,14 +25,24 @@ type Line struct {
 	Spacer string
 	Count  uint64
 	Text   string
+	Graph  string
 }
 
 func (c *CLI) Run(args []string) int {
 	var version bool
 	var max uint64
+	var total uint64
+	var totalFlag bool
+	var target uint64
+	var length int
+	var verbose bool
+
 	flags := flag.NewFlagSet("c2g", flag.ContinueOnError)
 	flags.SetOutput(c.errStream)
 	flags.BoolVar(&version, "version", false, "Print version information and quit")
+	flags.BoolVar(&totalFlag, "total", false, "Take a percentage from the total.")
+	flags.IntVar(&length, "length", 30, "Specify length of Graph.")
+	flags.BoolVar(&verbose, "verbose", false, "Verbose mode.")
 
 	if err := flags.Parse(args[1:]); err != nil {
 		return ExitCodeParseFlagError
@@ -52,9 +62,25 @@ func (c *CLI) Run(args []string) int {
 		if max < i.Count {
 			max = i.Count
 		}
+		total += i.Count
 	}
 
-	PrintGraph(c.outStream, lines, max)
+	switch {
+	case length > 100:
+		length = 100
+	case length < 10:
+		length = 10
+	}
+
+	if totalFlag {
+		target = total
+	} else {
+		target = max
+	}
+
+	GenGraph(lines, target, length, verbose)
+
+	PrintLine(c.outStream, lines)
 
 	return ExitCodeOK
 }
@@ -77,10 +103,38 @@ func Parser(stdin io.Reader, lines *[]Line) error {
 	return nil
 }
 
-func PrintGraph(stdout io.Writer, lines []Line, max uint64) {
+func GenGraph(lines []Line, target uint64, length int, verbose bool) {
+
+	n := len(lines)
+	if verbose {
+		for i := 0; i < n; i++ {
+			barCount := int(lines[i].Count * uint64(length) / target)
+			percentage := int(lines[i].Count * 100 / target)
+			charLen := len(strconv.Itoa(percentage))
+			var format strings.Builder
+			format.WriteString("[%s%")
+			format.WriteString(strconv.Itoa(charLen))
+			format.WriteString("d]")
+			var graph strings.Builder
+			graph.WriteString(strings.Repeat("|", barCount))
+			graph.WriteString(strings.Repeat(" ", length-barCount))
+			lines[i].Graph = fmt.Sprintf(format.String(), graph.String()[:length-len(strconv.Itoa(percentage))], percentage)
+		}
+	} else {
+		for i := 0; i < n; i++ {
+			barCount := int(lines[i].Count * uint64(length) / target)
+			var graph strings.Builder
+			graph.WriteString(strings.Repeat("|", barCount))
+			graph.WriteString(strings.Repeat(" ", length-barCount))
+			lines[i].Graph = fmt.Sprintf("[%s]", graph.String())
+		}
+	}
+}
+
+func PrintLine(stdout io.Writer, lines []Line) {
+
 	for _, line := range lines {
-		bar_count := int(line.Count * 30 / max)
-		fmt.Fprintf(stdout, "%s%d [%-30s] %s\n",
-			line.Spacer, line.Count, strings.Repeat("|", bar_count), line.Text)
+		fmt.Fprintf(stdout, "%s%d %s %s\n",
+			line.Spacer, line.Count, line.Graph, line.Text)
 	}
 }
--- c2go_test.go
+++ c2go_test.go
@@ -35,13 +35,15 @@ func TestRun_parseError(t *testing.T) {
 }
 
 func TestRun_printGraph(t *testing.T) {
-	stdin := bytes.NewBufferString(" 452 64.242.88.10\n" +
-		" 270 10.0.0.153\n" +
-		"  51 h24-71-236-129.ca.shawcable.net\n" +
-		"  44 cr020r01-3.sac.overture.com\n" +
-		"  32 h24-70-69-74.ca.shawcable.net\n")
+	var stdin bytes.Buffer
+	stdin.WriteString(" 452 64.242.88.10\n")
+	stdin.WriteString(" 270 10.0.0.153\n")
+	stdin.WriteString("  51 h24-71-236-129.ca.shawcable.net\n")
+	stdin.WriteString("  44 cr020r01-3.sac.overture.com\n")
+	stdin.WriteString("  32 h24-70-69-74.ca.shawcable.net\n")
+
 	outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
-	cli := &CLI{inStream: stdin, outStream: outStream, errStream: errStream}
+	cli := &CLI{inStream: &stdin, outStream: outStream, errStream: errStream}
 	args := strings.Split("c2g", " ")
 
 	status := cli.Run(args)
@@ -49,12 +51,97 @@ func TestRun_printGraph(t *testing.T) {
 		t.Errorf("ExitStatus=%d, want %d", status, ExitCodeOK)
 	}
 
-	expected := []byte(" 452 [||||||||||||||||||||||||||||||] 64.242.88.10\n" +
-		" 270 [|||||||||||||||||             ] 10.0.0.153\n" +
-		"  51 [|||                           ] h24-71-236-129.ca.shawcable.net\n" +
-		"  44 [||                            ] cr020r01-3.sac.overture.com\n" +
-		"  32 [||                            ] h24-70-69-74.ca.shawcable.net\n")
-	if bytes.Compare(expected, outStream.Bytes()) != 0 {
+	var expected bytes.Buffer
+	expected.WriteString(" 452 [||||||||||||||||||||||||||||||] 64.242.88.10\n")
+	expected.WriteString(" 270 [|||||||||||||||||             ] 10.0.0.153\n")
+	expected.WriteString("  51 [|||                           ] h24-71-236-129.ca.shawcable.net\n")
+	expected.WriteString("  44 [||                            ] cr020r01-3.sac.overture.com\n")
+	expected.WriteString("  32 [||                            ] h24-70-69-74.ca.shawcable.net\n")
+	if bytes.Compare(expected.Bytes(), outStream.Bytes()) != 0 {
+		t.Errorf("Output=%q, want %q", errStream.String(), expected)
+	}
+}
+
+func TestRun_printGraph_terget_total(t *testing.T) {
+	var stdin bytes.Buffer
+	stdin.WriteString(" 452 64.242.88.10\n")
+	stdin.WriteString(" 270 10.0.0.153\n")
+	stdin.WriteString("  51 h24-71-236-129.ca.shawcable.net\n")
+	stdin.WriteString("  44 cr020r01-3.sac.overture.com\n")
+	stdin.WriteString("  32 h24-70-69-74.ca.shawcable.net\n")
+
+	outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
+	cli := &CLI{inStream: &stdin, outStream: outStream, errStream: errStream}
+	args := strings.Split("c2g -total", " ")
+
+	status := cli.Run(args)
+	if status != ExitCodeOK {
+		t.Errorf("ExitStatus=%d, want %d", status, ExitCodeOK)
+	}
+
+	var expected bytes.Buffer
+	expected.WriteString(" 452 [|||||||||||||||               ] 64.242.88.10\n")
+	expected.WriteString(" 270 [|||||||||                     ] 10.0.0.153\n")
+	expected.WriteString("  51 [|                             ] h24-71-236-129.ca.shawcable.net\n")
+	expected.WriteString("  44 [|                             ] cr020r01-3.sac.overture.com\n")
+	expected.WriteString("  32 [|                             ] h24-70-69-74.ca.shawcable.net\n")
+	if bytes.Compare(expected.Bytes(), outStream.Bytes()) != 0 {
+		t.Errorf("Output=%q, want %q", errStream.String(), expected)
+	}
+}
+
+func TestRun_printGraph_length_50(t *testing.T) {
+	var stdin bytes.Buffer
+	stdin.WriteString(" 452 64.242.88.10\n")
+	stdin.WriteString(" 270 10.0.0.153\n")
+	stdin.WriteString("  51 h24-71-236-129.ca.shawcable.net\n")
+	stdin.WriteString("  44 cr020r01-3.sac.overture.com\n")
+	stdin.WriteString("  32 h24-70-69-74.ca.shawcable.net\n")
+
+	outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
+	cli := &CLI{inStream: &stdin, outStream: outStream, errStream: errStream}
+	args := strings.Split("c2g -length 50", " ")
+
+	status := cli.Run(args)
+	if status != ExitCodeOK {
+		t.Errorf("ExitStatus=%d, want %d", status, ExitCodeOK)
+	}
+
+	var expected bytes.Buffer
+	expected.WriteString(" 452 [||||||||||||||||||||||||||||||||||||||||||||||||||] 64.242.88.10\n")
+	expected.WriteString(" 270 [|||||||||||||||||||||||||||||                     ] 10.0.0.153\n")
+	expected.WriteString("  51 [|||||                                             ] h24-71-236-129.ca.shawcable.net\n")
+	expected.WriteString("  44 [||||                                              ] cr020r01-3.sac.overture.com\n")
+	expected.WriteString("  32 [|||                                               ] h24-70-69-74.ca.shawcable.net\n")
+	if bytes.Compare(expected.Bytes(), outStream.Bytes()) != 0 {
+		t.Errorf("Output=%q, want %q", errStream.String(), expected)
+	}
+}
+
+func TestRun_printGraph_verbose(t *testing.T) {
+	var stdin bytes.Buffer
+	stdin.WriteString(" 452 64.242.88.10\n")
+	stdin.WriteString(" 270 10.0.0.153\n")
+	stdin.WriteString("  51 h24-71-236-129.ca.shawcable.net\n")
+	stdin.WriteString("  44 cr020r01-3.sac.overture.com\n")
+	stdin.WriteString("  32 h24-70-69-74.ca.shawcable.net\n")
+
+	outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
+	cli := &CLI{inStream: &stdin, outStream: outStream, errStream: errStream}
+	args := strings.Split("c2g -verbose", " ")
+
+	status := cli.Run(args)
+	if status != ExitCodeOK {
+		t.Errorf("ExitStatus=%d, want %d", status, ExitCodeOK)
+	}
+
+	var expected bytes.Buffer
+	expected.WriteString(" 452 [|||||||||||||||||||||||||||100] 64.242.88.10\n")
+	expected.WriteString(" 270 [|||||||||||||||||           59] 10.0.0.153\n")
+	expected.WriteString("  51 [|||                         11] h24-71-236-129.ca.shawcable.net\n")
+	expected.WriteString("  44 [||                           9] cr020r01-3.sac.overture.com\n")
+	expected.WriteString("  32 [||                           7] h24-70-69-74.ca.shawcable.net\n")
+	if bytes.Compare(expected.Bytes(), outStream.Bytes()) != 0 {
 		t.Errorf("Output=%q, want %q", errStream.String(), expected)
 	}
 }
